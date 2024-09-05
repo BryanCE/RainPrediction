dfd672ebbceb5dc1841ecc8175f0e609d7c8c8b7
Bryan <bryanedman86@gmail.com>
2024-09-04 18:33:30 -0600

    Messing around

--- format.py
+++ format.py
@@ -21,6 +21,27 @@ def process_files(directory):
 
             print(f"Processed file: {filename}")
 
+def process_file(file):
+    if file.endswith('.json'):
+        year = int(file.split('_')[1].split('.')[0])
+        file_path = os.path.join(os.getcwd(), file)
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+
+        modified_data = []
+        for item in data:
+            item['Year'] = year
+            modified_data.append(item)
+
+        output_filename = f"precip_{year}.json"
+        output_file_path = os.path.join(os.getcwd(), output_filename)
+        with open(output_file_path, 'w') as file:
+            json.dump(modified_data, file, indent=4)
+
+        print(f"Processed file: {file}")
+
 if __name__ == '__main__':
-    directory = os.getcwd()
-    process_files(directory)
+    #directory = os.getcwd()
+    #directory = os.path.join(directory, 'backups/backups/rainfall_2013.json')
+    file = "backups/rainfall_2013.json"
+    process_file(file)
